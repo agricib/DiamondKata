f2529c180862038da32e3fc7d6a17c5d64dd9921
ROMANIA\BAgrici <bogdan-marian.agrici@dxc.com>
2024-04-30 17:29:16 +0300

    added example of failing test for RGRC approach

--- DiamondKata.Tests/DiamondKata.SimpleTests.cs
+++ DiamondKata.Tests/DiamondKata.SimpleTests.cs
@@ -1,4 +1,4 @@
-namespace DiamondKata.Tests
+﻿namespace DiamondKata.Tests
 {
     public class Given_A_Should_Return_A_Test
     {
@@ -13,6 +13,30 @@ namespace DiamondKata.Tests
 
             // Assert
             Assert.Equal("A", result);
+        }
+    }
+
+
+    public class Failing_Test_Given_B_Should_Return_B_Diamond_Test
+    {
+        [Fact]
+        public void Given_B_Should_Return_B_Diamond()
+        {
+            var input = 'B';
+
+            var result = DiamondPrinter.Print(input);
+
+            Assert.Equal(" A \nB B\n A ", result);
+
+        //             ↓ (pos 2)
+        //Expected: " A \nB B\n A "
+        //Actual: " A\nB B\n A"
+        //            ↑ (pos 2)
+
+            //In TDD there's 2 approaches, Red-Green-Refacor and Test-First Approach
+            //This should be a RedGreenRefactor cycle approach where you write the
+            //failing test and implement the simplest solution to make the test pass, then refactor the code
+            //while ensuring the test still passes, but for my solution I chose to not use trailling spaces
         }
     }
 
@@ -41,4 +65,5 @@ namespace DiamondKata.Tests
             Assert.Equal("    A\n   B B\n  C   C\n D     D\nE       E\n D     D\n  C   C\n   B B\n    A", result);
         }
     }
+
 }
